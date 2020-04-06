5d7c052d8cf4ef0c5e45d525eca00fc438fea220
Slavfox <slavfoxman@gmail.com>
2020-04-06 21:24:18 +0200

    propagate token

--- update.sh
+++ update.sh
@@ -3,4 +3,4 @@ git pull --recurse-submodules
 source .env
 poetry install
 poetry run python update_game_data.py
-screen -dmS rzepabot bash -c 'poetry run python -m rzepabot'
+screen -dmS rzepabot bash -c 'RZEPABOT_TOKEN=$RZEPABOT_TOKEN poetry run python -m rzepabot'
