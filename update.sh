ccf7909f1ce475d4fe6ce525b30c6f67b7e183da
Slavfox <slavfoxman@gmail.com>
2020-04-06 19:39:32 +0200

    Install deps before running

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 pyenv shell 3.8.2
 poetry run python update_game_data.py
-screen -dmS rzepabot bash -c 'pyenv shell 3.8.2; source .env; poetry run python -m rzepabot'
+screen -dmS rzepabot bash -c 'pyenv shell 3.8.2; poetry install; source .env; poetry run python -m rzepabot'
